0f46e83cc0c3b1d4833278bf7c8f843baa2f50b7
hisert <100667448+hisert@users.noreply.github.com>
2024-04-01 14:51:58 +0300

    Update oled.py

--- oled.py
+++ oled.py
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-cd
-pkill test
-rm -r test_cpp
-git clone https://github.com/hisert/test_cpp.git
-cd test_cpp/test
-g++ -o test main.cpp -lwiringPi
-/root/test_cpp/test/test
+import ssd1306
