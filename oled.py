b45523a762aade0091a0e5f0dc5413d552138b35
hisert <100667448+hisert@users.noreply.github.com>
2024-04-01 17:05:41 +0300

    Update oled.py

--- oled.py
+++ oled.py
@@ -433,6 +433,8 @@ def parse_data(data):
         data = data[data.index('(') + 1:data.index(')')]
         parts = data.split(',')
         temp_a = parts[0]
+        temp_b = ""
+        temp_c = ""
         if temp_a == "1":
           temp_b = parts[1]
           temp_c = parts[2]
